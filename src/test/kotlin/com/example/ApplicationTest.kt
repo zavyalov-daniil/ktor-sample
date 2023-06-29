ab6e221ac04be393cb1fbcf42d96b058f31d0e7d
Daniil Zavyalov <dezavv1@gmail.com>
2023-06-29 23:15:57 +0600

    Simple test

--- src/test/kotlin/com/example/ApplicationTest.kt
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -1,21 +1,26 @@
 package com.example
 
+import com.example.models.Customer
+import com.example.models.customerStorage
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.server.testing.*
 import kotlin.test.*
 import io.ktor.http.*
 import com.example.plugins.*
+import io.ktor.client.call.*
 
 class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
-        application {
-            configureRouting()
-        }
-        client.get("/").apply {
+        val customer = Customer("First", "aaa", "bbb", "eee")
+        customerStorage.add(customer)
+        client.get("/customer/First").apply {
             assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
+            assertEquals(
+                """{"id":"First","firstName":"aaa","lastName":"bbb","email":"eee"}""",
+                bodyAsText()
+            )
         }
     }
 }
